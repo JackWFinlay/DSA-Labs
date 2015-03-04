b2c0ebdc106813318e9a72a49826592a10280325
JackWFinlay <jfin404@aucklanduni.ac.nz>
2015-03-05 11:09:50 +1300

    Add lab 1

---
+++ Lab1/src/dsalab1/Ball.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsalab1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author fhp0351
+ */
+public class Ball extends JPanel implements Runnable {
+    
+    private double centre_x;
+    private double centre_y;
+    private double direction_x;
+    private double direction_y;
+    private double velocity;
+    private double r;
+    private Color color;
+    private Random rand;
+    private JFrame frame; 
+    private String name;
+    
+    // Overide default constructor
+    public Ball(){}
+    
+    public Ball(double centre_x, double centre_y, double direction_x, 
+            double direction_y, double velocity, double r){
+        
+        this.rand = new Random();
+        this.centre_x = centre_x;
+        this.centre_y = centre_y;
+        this.direction_x = direction_x;
+        this.direction_y = direction_y;
+        this.direction_x = direction_x;
+        this.direction_y = direction_y;
+        this.velocity = velocity;
+        this.r = r;
+        this.color = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
+        this.setBackground(new Color(0, 0, 0, 0));
+        this.setPreferredSize(new Dimension(BouncingBall.frame.getSize()));
+    }
+    
+    public double getCentre_x() {
+        return centre_x;
+    }
+
+    public double getCentre_y() {
+        return centre_y;
+    }
+
+    public double getDirection_x() {
+        return direction_x;
+    }
+
+    public double getDirection_y() {
+        return direction_y;
+    }
+
+    public double getVelocity() {
+        return velocity;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setPaint(color);
+        g2d.fill(new Ellipse2D.Double(centre_x, centre_y, r, r));
+        
+    }
+    
+    public void move(){
+    
+        double normalizedDirection_x = normalizeDirection(direction_x);
+        double normalizedDirection_y = normalizeDirection(direction_y);
+        
+        centre_x = (getCentre_x() + (velocity * normalizedDirection_x));
+        centre_y = (getCentre_y() + (velocity * normalizedDirection_y));
+        
+        if ((centre_x <= 0) || ((centre_x + r) >= this.getSize().width)) {
+            direction_x *= -1.0;
+            color = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
+        }
+        if ((centre_y <= 0) || ((centre_y + r) >= this.getSize().height)) {
+            direction_y *= -1.0;
+            color = new Color(rand.nextFloat(), rand.nextFloat(), rand.nextFloat());
+        }
+        System.out.printf("%s position (%f,%f), direction (%f,%f), velocity (%f), radius (%f) \n", Thread.currentThread().getName(),
+                    centre_x, centre_y, direction_x, direction_y, velocity, r );
+        
+    }
+    
+    private double normalizeDirection (double i) {
+        return (i/(Math.sqrt((Math.pow(direction_x,2))+(Math.pow(direction_y,2)))));
+    }
+    
+    public void run(){
+    while (true) {
+            move();
+            BouncingBall.frame.revalidate();
+            BouncingBall.frame.repaint();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(BouncingBall.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
