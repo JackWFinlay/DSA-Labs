b4d58ff79c11a20f4273f2bb82a126d95fa6d7d9
Jack Witbrock Finlay <jack.witbrock@gmail.com>
2015-03-13 16:12:51 +1300

    Add Main, fix implementation

---
+++ Lab1/Main.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Jack Witbrock Finlay, Jack.Witbrock@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args){
+        BouncingBall bouncingBall = new BouncingBall();
+        bouncingBall.createBall();
+        bouncingBall.createBall();
+        bouncingBall.open();
+
+    }
+
+}
